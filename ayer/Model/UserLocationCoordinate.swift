a73ead53c4150e51d36d285dc25d76ca213df63b
Ezaden <ezadens@yahoo.com>
2019-10-31 18:21:04 +0800

    Created struct for user location

---
+++ ayer/Model/UserLocationCoordinate.swift
@@ -0,0 +1,20 @@
+//
+//  UserLocationCoordinate.swift
+//  ayer
+//
+//  Created by Ezaden Seraj on 30/10/2019.
+//  Copyright © 2019 Ezaden Seraj. All rights reserved.
+//
+
+import Foundation
+
+struct UserLocationCoordinate {
+    
+    let latitude: Int
+    let longitude : Int
+    
+    init(latitude: Int, longitude: Int) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+}

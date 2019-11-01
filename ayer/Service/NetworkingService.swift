876669701cad7eba8c282534301e4b8688d6da2a
Ezaden <ezadens@yahoo.com>
2019-11-01 10:24:10 +0800

    Fetch data using combine framework and fetch icon data using closures

--- ayer/Service/NetworkingService.swift
+++ ayer/Service/NetworkingService.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-
+import Combine
 
 class NetworkingService {
     
@@ -40,6 +40,7 @@ class NetworkingService {
                 
                 if let data = data {
                     let decoder = JSONDecoder()
+                    decoder.dateDecodingStrategy = .secondsSince1970
                     
                     do {
                         let weatherList = try decoder.decode(WeatherData.self, from: data)
@@ -55,7 +56,8 @@ class NetworkingService {
         
     }
     
-    static func fetchWeatherData(userLocationCordinates: UserLocationCoordinate, completionHandler: @escaping Handler<WeatherData>) {
+    
+    static func fetchWeeklyWeatherData(userLocationCordinates: UserLocationCoordinate) -> AnyPublisher<WeeklyWeatherList, APIError> {
         
         let path = "/data/2.5/\(Query.forecast)"
         var endpoint = Endpoint(path: path)
@@ -63,28 +65,51 @@ class NetworkingService {
         let lat = String(userLocationCordinates.latitude)
         let lon = String(userLocationCordinates.longitude)
         
-        endpoint.queryItems.insert(URLQueryItem(name: "lat", value: lat), at: 0)
-        endpoint.queryItems.insert(URLQueryItem(name: "lon", value: lon), at: 1)
+        endpoint.queryItems.append(URLQueryItem(name: "lat", value: lat))
+        endpoint.queryItems.append(URLQueryItem(name: "lon", value: lon))
+        endpoint.queryItems.append(URLQueryItem(name: "units", value: TemperatureUnit.metric.rawValue))
+        
+        guard let url = endpoint.url else {
+            let error = APIError.networkError(description: "Couldn't create URL")
+            return Fail(error: error).eraseToAnyPublisher()
+        }
+        
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .secondsSince1970
+        
+        let task = urlSession.dataTaskPublisher(for: URLRequest(url: url)).mapError { error in
+            APIError.networkError(description: error.localizedDescription)
+        }
+        .flatMap(maxPublishers: .max(1)) { jsonResponse in
+            
+            Just(jsonResponse.data).decode(type: WeeklyWeatherList.self, decoder: decoder)
+                .mapError { error in
+                    APIError.parsingError(description: error.localizedDescription)
+            }.eraseToAnyPublisher()
+        }.eraseToAnyPublisher()
+        
+        return task
+        
+    }
+    
+    
+    
+    static func getIconData(iconName: String, completionHandler: @escaping Handler<Data>) {
+        let path = "/img/w/\(iconName).png"
+        var endpoint = Endpoint(path: path)
+        endpoint.queryItems.removeAll()
         
         guard let url = endpoint.url else {return}
         
-        let task = urlSession.dataTask(with: url) { (data, response, error) in
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else {
+                return
+            }
             
             DispatchQueue.main.async {
-                
-                if let data = data {
-                    let decoder = JSONDecoder()
-                    
-                    do {
-                        let weatherList = try decoder.decode(WeatherData.self, from: data)
-                        completionHandler(.success(weatherList))
-                    } catch {
-                        completionHandler(.failure(error))
-                    }
-                }
+                completionHandler(.success(data))
             }
-        }
-        
-        task.resume()
+        }.resume()
     }
+    
 }
